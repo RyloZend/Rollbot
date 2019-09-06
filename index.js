1bc600062a9bffd6f26bab1986b23d31935f44f8
RyloZend <anton.d.privat@gmail.com>
2019-09-06 23:40:30 +0200

    Added Jo Freunde

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ let emoji4 = message.guild.emojis.find('name', "smugnagisa");
     .then(message.react(emoji4));
   }
 
+  if(message.content.startsWith === "Jo Freunde") {
+
+    message.channel.send("Doppel Peace hier ist wieder euer Tanzverbot");
+
+    return true;
+  };
+
+
   if(cmd === `${prefix}love`){
     return message.channel.send("$mu")
   }
